9163722aa5e864701a64fe6f5dfaecfccbffb9c1
David Law <davidsamuellaw@gmail.com>
2018-10-12 21:38:02 -0700

    Add http request utilities

---
+++ solardat/http.py
@@ -0,0 +1,59 @@
+from requests.models import Response
+from typing import Dict, Optional
+import requests
+
+
+BASE_URL = "http://solardat.uoregon.edu"
+ARCHIVAL_PATH = "SelectArchival.html"
+LIST_FILES_PATH = "cgi-bin/ShowArchivalFiles.cgi"
+
+
+class _ResponseCache(object):
+    """Cache for HTTP responses."""
+
+    def __init__(self) -> None:
+        self._cache: Dict[str, Optional[Response]] = {}
+
+    def __getitem__(self, path: str) -> Optional[Response]:
+        if path not in self._cache:
+            self._cache[path] = None
+        return self._cache[path]
+
+    def __setitem__(self, path: str, value: Optional[Response]) -> None:
+        self._cache[path] = value
+
+    def get_etag(self, path: str) -> Optional[str]:
+        cached_response = self[path]
+        if cached_response is None:
+            return None
+        return cached_response.headers["ETag"]
+
+    def check_response(self, path: str, response: Response) -> Response:
+        if response.status_code != 304:
+            self._cache[path] = response
+            return response
+        cached_response = self._cache[path]
+        if cached_response is None:
+            raise RuntimeError
+        return cached_response
+
+_cache = _ResponseCache()
+_cache[ARCHIVAL_PATH] = None
+_cache[LIST_FILES_PATH] = None
+
+def dispatch(method: str, path: str, **kwds) -> Response:  # noqa: E501
+    if method not in ("GET", "POST"):
+        raise ValueError
+
+    url = f"{BASE_URL}/{path}"
+    etag = _cache.get_etag(path)
+
+    kwds = kwds.copy()
+    kwds["headers"] = {
+        **kwds.get("headers", {}),
+        "If-None-Match": etag,
+    }
+
+    response = requests.request(method, url, **kwds)
+    response.raise_for_status()
+    return _cache.check_response(path, response)
---
+++ tests/test_http.py
@@ -0,0 +1,104 @@
+from requests.exceptions import HTTPError
+from requests.models import Response
+import pytest
+import responses
+
+from solardat.http import _ResponseCache, _cache, BASE_URL, dispatch
+
+
+def empty_cache():
+    return _ResponseCache(), None
+
+def populated_cache():
+    cache = _ResponseCache()
+    response = Response()
+    response.headers = {"ETag": "etag"}
+    cache._cache["path/resource"] = response
+    return cache, response
+
+@pytest.fixture
+def setup_cache():
+    response = Response()
+    response.headers = {"ETag": "etag"}
+    response.status_code = 200
+    _cache["test"] = response
+    yield
+    del _cache._cache["test"]
+
+
+class TestResponseCache(object):
+    path = "path/resource"
+
+    def test_getitem_present(self):
+        cache, response = populated_cache()
+
+        out = cache[self.path]
+        assert out is response
+
+    def test_getitem_missing(self):
+        cache, response = empty_cache()
+
+        out = cache[self.path]
+        assert out is response
+        # Attempted access adds as null.
+        assert cache._cache[self.path] is None
+
+    def test_setitem(self):
+        response = Response()
+        cache = _ResponseCache()
+
+        cache[self.path] = response
+        assert cache._cache.get(self.path) is response
+
+    def test_get_etag_present(self):
+        cache, response = populated_cache()
+
+        out = cache.get_etag(self.path)
+        assert out is "etag"
+
+    def test_get_etag_missing(self):
+        cache, response = empty_cache()
+
+        out = cache.get_etag(self.path)
+        assert out is response
+
+class TestCacheSetup(object):
+    def test_paths_in_cache(self):
+        assert "SelectArchival.html" in _cache._cache
+        assert "cgi-bin/ShowArchivalFiles.cgi" in _cache._cache
+
+def callback(request):
+    # `requests` strips out headers with null keys.
+    etag = request.headers.get("If-None-Match")
+    if etag == "etag":
+        return (304, {}, "")
+
+    headers = {"ETag": "etag"}
+    return (200, headers, "")
+
+class TestDispatch(object):
+    path = "test"
+
+    @pytest.mark.parametrize("method", ["DELETE", "HEAD", "OPTIONS", "PATCH", "PUT"])
+    @responses.activate  # Make sure there's no outbound traffic.
+    def test_validates_method(self, method):
+        with pytest.raises(ValueError):
+            dispatch(method, self.path)
+
+    @responses.activate
+    def test_from_cache(self, setup_cache):
+        responses.add_callback(responses.GET, f"{BASE_URL}/test", callback=callback)
+        response = dispatch("GET", self.path)
+        assert response is _cache[self.path]
+
+    @responses.activate
+    def test_request(self):
+        responses.add_callback(responses.GET, f"{BASE_URL}/test", callback=callback)
+        response = dispatch("GET", self.path)
+        assert response is _cache[self.path]
+
+    @responses.activate
+    def test_raises(self):
+        responses.add(responses.GET, f"{BASE_URL}/non-existent", status=400)
+        with pytest.raises(HTTPError):
+            dispatch("GET", "non-existent")
